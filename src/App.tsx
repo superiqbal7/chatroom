522800fc95671f17db2c60c8d65c0d39d47dc4c8
rahim iqbal <superiqu@gmail.com>
2021-01-18 20:05:18 +0600

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 // @ts-ignore
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import CreateRoom from './pages/CreateRoom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CreateRoom from "./pages/CreateRoom";
+import ChatRoom from "./pages/ChatRoom";
 
 function App() {
   return (
@@ -16,7 +12,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" exact component={CreateRoom} />
-          <Route path="/room/:roomID" component={CreateRoom} />
+          <Route path="/room/:roomID" component={ChatRoom} />
         </Switch>
       </Router>
     </div>
--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,73 +1,246 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 50%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Messages = styled.div`
+  width: 100%;
+  height: 60%;
+  border: 1px solid black;
+  margin-top: 10px;
+  overflow: scroll;
+`;
+
+const MessageBox = styled.textarea`
+  width: 100%;
+  height: 30%;
+`;
+
+const Button = styled.div`
+  width: 50%;
+  border: 1px solid black;
+  margin-top: 15px;
+  height: 5%;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: black;
+  color: white;
+  font-size: 18px;
+`;
+
+const MyRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 10px;
+`;
+
+const MyMessage = styled.div`
+  width: 45%;
+  background-color: blue;
+  color: white;
+  padding: 10px;
+  margin-right: 5px;
+  text-align: center;
+  border-top-right-radius: 10%;
+  border-bottom-right-radius: 10%;
+`;
+
+const PartnerRow = styled(MyRow)`
+  justify-content: flex-start;
+`;
+
+const PartnerMessage = styled.div`
+  width: 45%;
+  background-color: grey;
+  color: white;
+  border: 1px solid lightgray;
+  padding: 10px;
+  margin-left: 5px;
+  text-align: center;
+  border-top-left-radius: 10%;
+  border-bottom-left-radius: 10%;
+`;
 
 const ChatRoom = (props: any) => {
-  function create() {
-    let offer: any;
-    let answer: any;
+  const peerRef: any = useRef();
+  const sendChannel: any = useRef();
+  const [text, setText] = useState("");
+  const [messages, setMessages]: any = useState([]);
+  const socketRef: any = useRef();
+  const otherUser: any = useRef();
 
-    const create = () => {
-      const peer = new RTCPeerConnection();
+  useEffect(() => {
+    socketRef.current = io("http://localhost:8000");
+    socketRef.current.emit("join room", props.match.params.roomID);
 
-      const dc = peer.createDataChannel("channel");
+    socketRef.current.on("other user", (userID: any) => {
+      callUser(userID);
+      otherUser.current = userID;
+    });
 
-      dc.onmessage = (e) => {
-        console.log("new message: " + e);
-      };
+    socketRef.current.on("user joined", (userID: any) => {
+      otherUser.current = userID;
+    });
 
-      dc.onopen = (e) => {
-        console.log("Connection opened!");
-      };
-      peer.onicecandidate = (e) => {
-        "New Ice Candidate! reprinting SDP" +
-          JSON.stringify(peer.localDescription);
-        offer = peer.localDescription;
-      };
+    socketRef.current.on("offer", handleOffer);
 
-      peer
-        .createOffer()
-        .then((o) => {
-          peer.setLocalDescription(o);
-        })
-        .then((a) => {
-          console.log("set successfully");
-        });
+    socketRef.current.on("answer", handleAnswer);
 
-      peer.setRemoteDescription(answer);
-      dc.send("peer");
-    };
+    socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
+  }, []);
 
-    const join = () => {
-      const rc = new RTCPeerConnection();
-      let rcdc: any;
-      rc.onicecandidate = (e) => {
-        "New Ice Candidate! reprinting SDP" +
-          JSON.stringify(rc.localDescription);
-        answer = rc.localDescription;
-      };
+  function callUser(userID: string) {
+    peerRef.current = createPeer(userID);
 
-      rc.ondatachannel = (e) => {
-        rcdc = e.channel;
-        rcdc.onmessage = (msg: any) => {
-          console.log("new message: " + msg.data);
-        };
-        rcdc.onopen = (e: any) => {
-          console.log("connection opened");
-        };
-      };
-
-      rc.setRemoteDescription(offer).then((a) => console.log("offer set"));
-
-      rc.createAnswer()
-        .then((a) => {
-          rc.setLocalDescription(a);
-          answer = a;
-        })
-        .then((a) => console.log("answer created"));
-      rcdc.send("hola");
-    };
+    sendChannel.current = peerRef.current.createDataChannel("sendChannel");
+    sendChannel.current.onmessage = handleReceiveMessage;
   }
 
-  return <div></div>;
+  function createPeer(userID?: string) {
+    const configuration = {
+      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    };
+    const peerConnection = new RTCPeerConnection(configuration);
+
+    peerConnection.onicecandidate = handleICECandidateEvent;
+    peerConnection.onnegotiationneeded = () =>
+      handleNegotiationNeededEvent(userID);
+
+    return peerConnection;
+  }
+
+  function handleICECandidateEvent(e: any) {
+    if (e.candidate) {
+      const payload = {
+        target: otherUser.current,
+        candidate: e.candidate,
+      };
+      socketRef.current.emit("ice-candidate", payload);
+    }
+  }
+
+  function handleNegotiationNeededEvent(userID: any) {
+    peerRef.current
+      .createOffer()
+      .then((offer: any) => {
+        return peerRef.current.setLocalDescription(offer);
+      })
+      .then(() => {
+        const payload = {
+          target: userID,
+          caller: socketRef.current.id,
+          sdp: peerRef.current.localDescription,
+        };
+        socketRef.current.emit("offer", payload);
+      })
+      .catch((e: any) => console.log(e));
+  }
+
+  function handleReceiveMessage(e: any) {
+    setMessages((message: []) => [
+      ...message,
+      {
+        yours: false,
+        value: e.data,
+      },
+    ]);
+  }
+
+  function handleOffer(incoming: any) {
+    peerRef.current = createPeer();
+
+    peerRef.current.ondatachannel = (event: any) => {
+      sendChannel.current = event.channel;
+      sendChannel.current.onmessage = handleReceiveMessage;
+    };
+
+    const desc = new RTCSessionDescription(incoming.sdp);
+    peerRef.current
+      .setRemoteDescription(desc)
+      .then(() => {})
+      .then(() => {
+        return peerRef.current.createAnswer();
+      })
+      .then((answer: any) => {
+        return peerRef.current.setLocalDescription(answer);
+      })
+      .then(() => {
+        const payload = {
+          target: incoming.caller,
+          caller: socketRef.current.id,
+          sdp: peerRef.current.localDescription,
+        };
+        socketRef.current.emit("answer", payload);
+      });
+  }
+
+  function handleAnswer(message: any) {
+    const desc = new RTCSessionDescription(message.sdp);
+    peerRef.current
+      .setRemoteDescription(desc)
+      .catch((e: any) => console.log(e));
+  }
+
+  function handleNewICECandidateMsg(incoming: any) {
+    const candidate = new RTCIceCandidate(incoming);
+
+    peerRef.current
+      .addIceCandidate(candidate)
+      .catch((e: any) => console.log(e));
+  }
+
+  function handleChange(e: any) {
+    setText(e.target.value);
+  }
+
+  function sendMessage() {
+    sendChannel.current.send(text);
+    setMessages((messages: any) => [
+      ...messages,
+      {
+        yours: true,
+        value: text,
+      },
+    ]);
+    setText("");
+  }
+
+  function renderMessage(message: any, index: any) {
+    if (message.yours) {
+      return (
+        <MyRow key={index}>
+          <MyMessage>{message.value}</MyMessage>
+        </MyRow>
+      );
+    }
+
+    return (
+      <PartnerRow key={index}>
+        <PartnerMessage>{message.value}</PartnerMessage>
+      </PartnerRow>
+    );
+  }
+
+  return (
+    <Container>
+      <Messages>{messages.map(renderMessage)}</Messages>
+      <MessageBox
+        value={text}
+        onChange={handleChange}
+        placeholder="Say something....."
+      />
+      <Button onClick={sendMessage}>Send..</Button>
+    </Container>
+  );
 };
 
 export default ChatRoom;
