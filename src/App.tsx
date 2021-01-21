ae781ad86d5e16112cba0be3c6dfc0ce0531fac7
Rahim Iqbal <superiqu@gmail.com>
2021-01-21 14:43:39 +0600

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 // @ts-ignore
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import CreateRoom from "./pages/CreateRoom";
 import ChatRoom from "./pages/ChatRoom";
 
 function App() {
@@ -11,7 +9,6 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/room/" exact component={CreateRoom} />
           <Route path="/" component={ChatRoom} />
         </Switch>
       </Router>
